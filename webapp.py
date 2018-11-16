e86ff7187dddae6a87b351a59a0fb6011a79706d
TarheK <42652896+TarheK@users.noreply.github.com>
2018-11-16 10:45:17 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -5,10 +5,6 @@ app = Flask(__name__)
 @app.route("/")
 def render_home():
     return render_template('home.html')
-@app.route("/responseh")
-def render_responseh():
-    millimeters = float(request.args['millimeters'])
-    response=millimeters * (1/10)
-    return render_template('response.html', response = response)
+  
 if __name__=="__main__":
     app.run(debug=False, port=54321)
